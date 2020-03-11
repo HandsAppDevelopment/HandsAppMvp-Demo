7ff83054a65898680a90b7fb8efa94cfa4588f93
Никита Красавин <6nike6@bk.ru>
2020-03-11 09:00:27 +0300

    init project

---
+++ HandsAppMvp-Demo/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  HandsAppMvp-Demo
+//
+//  Created by Никита Красавин on 11.03.2020.
+//  Copyright © 2020 Никита Красавин. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Podfile
@@ -0,0 +1,12 @@
+use_frameworks!
+inhibit_all_warnings!
+
+platform :ios, '12.0'
+
+target 'HandsAppMvp-Demo' do
+    # UI
+    pod 'HandsAppUI'
+
+    # Networking
+    pod 'Moya'q
+end
